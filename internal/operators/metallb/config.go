e32a29ef971d6d2bbb18181352b7436fc4b10912
root <root@edge-15.edge.lab.eng.rdu2.redhat.com>
2025-07-07 08:10:46 -0400

    MGMT-14822: Add MetalLB operator

---
+++ internal/operators/metallb/config.go
@@ -0,0 +1,8 @@
+package metallb
+
+const (
+	operatorName             string = "metallb"
+	operatorSubscriptionName string = "redhat-metallb-operator"
+	operatorNamespace        string = "openshift-metallb"
+	OperatorFullName         string = "MetalLB"
+)
